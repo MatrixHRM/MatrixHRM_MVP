63ee2682f30471c6db4e173e2d852c01db683ae1
RemyLeRat <ratishonok@gmail.com>
2024-10-06 18:48:45 +0300

    Up-to-date

--- server.js
+++ server.js
@@ -1,26 +1,5 @@
 const express = require('express');
 const mongoose = require('mongoose');
-<<<<<<< HEAD
-const dotenv = require('dotenv');
-
-dotenv.config();
-
-const app = express();
-const port = process.env.PORT || 5000;
-
-app.use(express.json());
-
-mongoose.connect(process.env.MONGO_URI)
-.then(() => console.log('MongoDB connected!'))
-.catch((err) => console.log(err));
-
-app.get('/', (req, res) => {
-  res.send('Hello, MatrixHRM!');
-});
-
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-=======
 require('dotenv').config();
 
 const app = express();
@@ -50,5 +29,4 @@ app.get('/', (req, res) => {
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
->>>>>>> Matrix-Skills
 });
